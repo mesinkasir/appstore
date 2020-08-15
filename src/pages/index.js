d3f9bdcec74a22337b56b8efd8fc2de33b1e4b7d
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2020-08-15 09:21:08 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,14 +22,14 @@ const Index = ({ data, location }) => {
  </div>
  <div class="col-12 col-md-1"></div>
      <div class="col-12 col-md-6 p-3 p-md-3">
-    <div class="shadow rounded p-3">
+    <div class="shadow rounded p-5">
 <h3><a title="aplikasi" href="/">App Store</a></h3>
 <img class="img-fluid" alt="aplikasi kasir toko" src="/img/aplikasi.jpg"/>
 <p>Offline desktop mode dalam kebutuhan stabilitas operasional dan free selamanya pada device hardware yang sama , atau via online mode dengan biaya per tahun yang sangat murah,semua tersedia untuk mu.</p>
 <Link class="button float-right" to="/applikasi">App Info >></Link>
 </div></div>
 <div class="col-12 col-md-6 p-3 p-md-3">
-        <div class="shadow rounded p-3">
+        <div class="shadow rounded p-5">
   <h3><a title="mesin kasir point of sale" href="/">Point Of Sale</a></h3>
 <img class="img-fluid" alt="mesin kasir" src="/img/mesinkasir.png"/>
 <p>Paket point of sale set lengkap mesin kasir terbaru modern siap digunakan dalam menunjang berbagai usaha dan bisnis free instalasi setting siap pakai menjadi sempurna,semua lengkap disini.</p>
