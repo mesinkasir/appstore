11e67153fb4a20ad5e83577d772c9da10938a741
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2020-08-15 09:36:33 +0700

    Update applikasi.js

--- src/pages/applikasi.js
+++ src/pages/applikasi.js
@@ -32,6 +32,24 @@ const Applikasi = ({ data, location }) => {
 <p class="text-left">Special untuk kebutuhan office dan bisnis dalam kebutuhan mencatat inventori stok , penerbitan invoice faktur hingga akuntansi.</p>
 <Link class="button btn-block" to="/office">More info</Link>
 </div>
+    <div class="col-12 col-md-4 p-3 p-md-3">
+  <h3><strong><a href="/applikasi" title="website modern technology">Website</a></strong></h3>
+<img class="img-fluid" alt="Pembuatan website modern terbaru" width="200" src="/img/pembuatanwebsitemodern.jpg"/> 
+<p class="text-left">Bangun dan buat website dengan technology modern bersama kami, aplikasi pembuatan website modern tersedia untuk bisnis mu.</p>
+<Link class="button btn-block" to="/toko">More info</Link>
+</div>
+<div class="col-12 col-md-4 p-3 p-md-3">
+<h3><strong><a href="/applikasi" title="aplikasi android apk">APK</a></strong></h3>
+<img class="img-fluid" alt="pembuatan APK APP" src="/img/pembuatanapk.jpg"/>
+<p class="text-left">Solusi pembuatan APK APPs modern dengan native version, mudah untuk di upload ke google & APP play store, dengan ID mu.</p>
+<Link class="button btn-block" to="/restoran">More info</Link>
+</div>
+<div class="col-12 col-md-4 p-3 p-md-3">
+<h3><strong><a href="/applikasi" title="aplikasi online shop">SHOP</a></strong></h3>
+<img class="img-fluid" alt="Pembuatan toko online sohp" width="200" src="/img/pembuatantokoonlineshop.png"/> 
+<p class="text-left">Full online shop dengan website + aplikasi kasir penjualan dan APK dalam build online shop integration untuk menunjang usaha.</p>
+<Link class="button btn-block" to="/office">More info</Link>
+</div>
 <div class="col-12 col-md-12 p-3 p-md-5 text-center bg-light">
   <p><strong>Mobile technology</strong></p>
 <p>All in one dengan online mode support berbagai os <a href="https://www.android.com/" target="blank" title="android">android</a> <a href="https://www.apple.com/id/iphone/" target="blank" title="android">iphone</a> windows maupun kebutuhan untuk offline mode tanpa biaya bulanan untuk kebutuhan mesin kasir mu.</p>
