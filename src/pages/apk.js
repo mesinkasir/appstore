d1472203aedb7675e498de606966dfaccb17acac
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2020-08-16 15:47:30 +0700

    Create apk.js

---
+++ src/pages/apk.js
@@ -0,0 +1,63 @@
+import React from "react"
+import { Link } from "gatsby"
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Apk = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="Pembuatan APK android APP ios googleplaystore" />
+      <Bio />
+      <h1><strong><a title="Develope android APK native" href="/website">Develope APK Native</a></strong></h1>
+<p>Pembuatan APK android APP ios untuk upload ke googleplaystore.</p>
+<div class="row text-center">
+<div class="col-12 col-md-12 p-3 p-md-3">
+<img class="img-fluid" width="100%" alt="pembuatan apk android murah" src="/img/jasa pembuatan app dan apk murah.png"/>
+<p>
+pembuatan native APK untuk kemudahan dalam develope APK android atau APP ios memberikan kemudahan untuk semakin dekat dengan pelanggan, native APK develope menggunakan flutter dan dart menjadikan kesempurnaan untuk menggunakan aplikasi android mu.
+</p>
+<h3><strong><a title="Applikasi toko kasir online web apps" href="/website">REAL APK</a></strong></h3>
+<p>
+Pembangunan pada project native APK full untuk APK android yang memudahkan dalam kebutuhan upload ke google play store dengan ID dan account usaha mu, semakin memudahkan dalam kebutuhan untuk memberikan keterangan pada APK sesuai dengan kebutuhan mu, mulai judul, deskripsi bisa kamu tulis sesuai deskripsi usaha , dengan kemudahan ini memberikan kebebasan dalam build APK .
+    </p>
+    <p>
+Pada APK android native by default ini tersedia fitur 4 kategori dan 4 penjelasan produk maupun jasa memudahkan pelanggan untuk donwload APK via playstore dan membaca informasi mengenai produk dan jasa pada usaha mu, semakin dekat dengan pelanggan dengan native APK. unutk kebutuhan penambahan fitur penjelasan produk tersedia dengan harga tejangkau, dan tersedia fitur integrasi APK + website + online shop dan aplikasi kasir point of sale dalam satu based.
+    </p>
+    </div>
+<div class="col-12 col-md-12 p-3 p-md-5">
+<a class="button" href="https://mesinkasir.github.io/e-catalog/dexopos%20shop.pdf" target="blank">Download</a> <a class="button" href="https://www.youtube.com/watch?v=1PWUy4woxSM&t=11s" target="blank">Mobile</a> <a class="button" href="https://www.youtube.com/playlist?list=PLQDm6k9_HvYMuYq7kJSmSGzF9SHvh_UWt" target="blank">Video</a> <a class="button" href="https://wa.me/6285646104747" target="blank" rel="nofollow">Chat Now</a> <a class="button" href="tel:+6285331361404" target="blank" rel="nofollow">Call Us</a>.
+<br/><br/><Link class="float-left" to="/onlineshop">&larr; Online Shop</Link> <Link class="float-right" to="/website">Website &rarr;</Link>
+</div>
+</div>
+    </Layout>
+  )
+}
+
+export default Website
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
